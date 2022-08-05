4908a502f6d444ffa96e2784b50986991a99419d
gernavarrete <germannavarrete.info@gmail.com>
2022-08-04 22:05:06 -0300

    Homework 02-JS Terminado

--- FT-M1/FT-M1/01-IntroToCS/homework/homework.js
+++ FT-M1/FT-M1/01-IntroToCS/homework/homework.js
@@ -2,24 +2,31 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  /*if (typeof num === 'string') {
   let sum = 0;
   for(let i = 0; i < num.length ; i++) {
     sum = sum + (num[i] * (2 **(num.length - 1 - i)));
   }
   return sum;
+  }
+  else {
+  return 'No es String'
+  }*/
+  return num.split("").reduce((a,v,i,arr) => a += 2 ** (arr.length - 1 - i) * v,0 );
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
   let bin = '';
   while (num >= 1) {
-    let u = num % 2;
+    bin = (num % 2) + bin;
     num = Math.floor(num / 2);
-    bin = u + bin;
   }
   return bin;
 }
 //hola
+
+  //"start": "nodemon homework.js" cambios en vivo del codigo instalar nodemon tmb
 
 module.exports = {
   BinarioADecimal,
