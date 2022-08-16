a4a5326470634e7123987b5dcd742b1010be732d
gernavarrete <germannavarrete.info@gmail.com>
2022-08-16 19:28:47 -0300

    04-EstruturaDeDatos-I home completo

--- FT-M1/FT-M1/04-EstructuraDeDatos-I/homework/homework.js
+++ FT-M1/FT-M1/04-EstructuraDeDatos-I/homework/homework.js
@@ -15,23 +15,75 @@ Como ejercicio adicional y completamente opcional, al terminar de resolver este 
 */
 
 function nFactorial(n) {
+  return (n < 3) ? n :  n * nFactorial(n - 1);
 }
 
 function nFibonacci(n) {
+  return (n < 0) ? 'Error' : (n === 0) ? 0 : (n === 1) ? 1 : nFibonacci(n - 1) + nFibonacci(n - 2);
 }
 
 /*
-Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde el primer elemento que ingresa es el primero que se quita. Definir los siguientes métodos:
+Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde el primer elemento que 
+ingresa es el primero que se quita. Definir los siguientes métodos:
   - enqueue: agrega un valor respetando el orden.
   - dequeue: remueve un valor respetando el orden. Retorna undefined cuando la queue está vacía.
   - size: retorna el tamaño (cantidad de elementos) de la queue.
 
 Pueden utilizar class o función constructora.
 */
-
 function Queue() {
+  this.array = [];    // funcion constructora....o tipo clase
+} 
 
+
+ Queue.prototype.enqueue = function(array){
+   return this.array.push(array);            // encola elementos al principio de la cola,
+ }
+ Queue.prototype.dequeue= function(array){
+   return this.array.shift(array);
+ }
+
+ Queue.prototype.size= function(array){
+  return this.array.length;
+ }
+
+/* function Queue() {
+  this.top = null,
+  this.length = 0
+} */
+/* function Node(value) {
+  this.value = value,
+  this.next = null
 }
+
+Queue.prototype.enqueue = function(value){
+  let node = new Node(value);
+  if (this.top === null) {
+    this.top = node;
+  } else {
+    let current = this.top.next;
+    console.log('este es el ',current);
+    while (current === null) {
+      current = this.next;
+      console.log(current);
+    }
+    //let aux = this.top.next;
+    current = node;
+    console.log(current);
+  }
+  this.length += 1;
+}
+Queue.prototype.dequeue = function() {
+  if (this.top === null) {
+    return undefined;
+  }else {
+    let aux = this.
+  }
+}
+Queue.prototype.size = function() {
+  return this.length;
+} */
+
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
