93e7c3a12ccaee08a61b4deb66bbfb9662b4bef2
gernavarrete <germannavarrete.info@gmail.com>
2022-10-02 09:49:24 -0300

    07-React-Style completed

--- FT-M2/FT-M2/07-React-Estilos/homework/src/components/Card.jsx
+++ FT-M2/FT-M2/07-React-Estilos/homework/src/components/Card.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
+import Cards from './Cards.jsx';
+import s from './Card.module.css';
 
 export default function Card(props) {
   // acá va tu código
-  return <div>Card Component</div>
+  const {img,max,min,name,onClose} = props;
+  return <div className={s.div}>
+            <button onClick={onClose} className={s.button}>X</button>
+            <h3>{name}</h3>
+            <div className={s.divTempContainer}>
+              <div className={s.divTemp}>
+                <h5>Min</h5>
+                <p>{min}</p>
+              </div>
+              <div className={s.divTemp}>
+                <h5>Max</h5>
+                <p>{max}</p>
+              </div>
+              <div><img src={`http://openweathermap.org/img/wn/${img}@2x.png`}  className={s.styleImg}/></div>
+            </div>
+          </div>
 };
--- FT-M2/FT-M2/07-React-Estilos/homework/src/components/SearchBar.jsx
+++ FT-M2/FT-M2/07-React-Estilos/homework/src/components/SearchBar.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import style from './SearchBar.module.css';
 
-export default function SearchBar(props) {
+export default function SearchBar({ onSearch }) {
   // acá va tu código
-  return <div>Search Bar Component</div>
+  let valueInput = 'London'
+   
+  return <div className={style.divContainer}>
+          <input type="text" placeholder='Ciudad...'/>
+          <button onClick={()=> { onSearch(valueInput)} } className={style.button} >Agregar</button>
+        </div>
 };
